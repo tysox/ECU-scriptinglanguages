5ed9034ced8f1924b12dc9ec2a77e4c27dcdf39d
Tyson <battersby+git@protonmail.com>
2021-07-13 02:03:49 +0800

    added explanatory comment

--- portfolio/week1/veracrypt_mount.sh
+++ portfolio/week1/veracrypt_mount.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 veracrypt --filesystem=none /dev/sdb
-# It should be noted that in the followin line "user" should be replaced with the username.
+# It should be noted that in the following line "user" should be replaced with the username.
 sudo mount -o umask=000 /dev/mapper/veracrypt1 /home/user/veracrypt1
 exit 0
