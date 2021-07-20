c7526e50fd5d493fb8b58f49bd2dc4061675153a
Tyson <battersby+git@protonmail.com>
2021-07-21 00:50:59 +0800

    Clean up, no functional changes.
    veracrypt_mount.sh was made more legible with comments added.
    Duplicate foldercopier.sh in root dir removed.

--- portfolio/week1/veracrypt_mount.sh
+++ portfolio/week1/veracrypt_mount.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#
+#Decrypts the drive (after asking for and receiving the correct password, which still appears on screen as a prompt) but doesn't mount it.
 veracrypt --filesystem=none /dev/sdb
-# It should be noted that in the following line "$(whoami)" will be replaced with the current user's username, or alternatively, you can just replace it with your usename.
+#
+#Tells the system to mount the drive with full access from the first path to the second path.
+#It should be noted that in the following line "$(whoami)" will be replaced with the current user's username, or alternatively, you can just replace it with your usename.
 sudo mount -o umask=000 /dev/mapper/veracrypt1 /home/$(whoami)/veracrypt1
 exit 0
